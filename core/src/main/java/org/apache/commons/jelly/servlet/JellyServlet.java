6298deb518d7781ea6b6ee9fe9a2ce8bc0a78742
Basil Crow <me@basilcrow.com>
2024-09-03 08:19:53 -0700

    [JENKINS-73256] Migrate Jelly from EE 8 to EE 9 (#114)

--- core/src/main/java/org/apache/commons/jelly/servlet/JellyServlet.java
+++ core/src/main/java/org/apache/commons/jelly/servlet/JellyServlet.java
@@ -23,11 +23,11 @@ import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.apache.commons.jelly.JellyContext;
 import org.apache.commons.jelly.JellyException;
